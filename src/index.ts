f172cecce8078ce4781b241e2e68c1592e868429
krisb <krisanathep.b@gli.co.th>
2021-04-12 11:36:45 +0700

    add webhook

--- src/index.ts
+++ src/index.ts
@@ -18,20 +18,20 @@ const app = express();
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
-// const lineClient = new Client({
-//   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-// });
+const lineClient = new Client({
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+});
 
-// const web3Service = new Web3Service();
-// const priceService = new PriceService();
-// const masterchefAddress = '0x73feaa1eE314F8c655E354234017bE2193C9E24E';
-// const contract = web3Service.getContract(MasterChef.abi, masterchefAddress);
-// const helper = new TokenHelper(web3Service, priceService);
-// const masterchef = new Masterchef(contract, helper);
+const web3Service = new Web3Service();
+const priceService = new PriceService();
+const masterchefAddress = '0x73feaa1eE314F8c655E354234017bE2193C9E24E';
+const contract = web3Service.getContract(MasterChef.abi, masterchefAddress);
+const helper = new TokenHelper(web3Service, priceService);
+const masterchef = new Masterchef(contract, helper);
 
 app.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>Hello my demo linexswap</h1>');
   res.end();
 });
 
@@ -39,48 +39,48 @@ app.get('/test', (req, res) => {
   res.status(200).json('Test Succeed');
 })
 
-// app.post('/webhook', async (req, res) => {
-//   const event = _.get(req, 'body.events[0]');
-//   const eventType = _.get(event, 'message.type');
-//   const message = _.get(event, 'message.text');
-//   const replyToken = _.get(event, 'replyToken') as string;
+app.post('/webhook', async (req, res) => {
+  const event = _.get(req, 'body.events[0]');
+  const eventType = _.get(event, 'message.type');
+  const message = _.get(event, 'message.text');
+  const replyToken = _.get(event, 'replyToken') as string;
 
-//   if (eventType !== 'text' || !isValidAddress(message)) {
-//     await lineClient.replyMessage(replyToken, {
-//       type: 'text',
-//       text:
-//       'Please input valid BSC address. For example, 0x3c74c735b5863c0baf52598d8fd2d59611c8320f 🐳',
-//     } as any);
-//     return res.sendStatus(200);
-//   }
+  if (eventType !== 'text' || !isValidAddress(message)) {
+    await lineClient.replyMessage(replyToken, {
+      type: 'text',
+      text:
+      'Please input valid BSC address. For example, 0x3c74c735b5863c0baf52598d8fd2d59611c8320f 🐳',
+    } as any);
+    return res.sendStatus(200);
+  }
 
-//   const address = message;
-//   const stakings = await masterchef.getStaking(pools, address);
-//   const positions = _.sortBy(
-//     stakings.map(stake => getPositions(stake)),
-//     ['totalValue']
-//   ).reverse();
-//   const totalValue = positions.reduce((sum, position) => sum + position.totalValue, 0);
+  const address = message;
+  const stakings = await masterchef.getStaking(pools, address);
+  const positions = _.sortBy(
+    stakings.map(stake => getPositions(stake)),
+    ['totalValue']
+  ).reverse();
+  const totalValue = positions.reduce((sum, position) => sum + position.totalValue, 0);
 
-//   await lineClient.replyMessage(replyToken, {
-//     type: 'flex',
-//     altText: 'Pancake Staking',
-//     contents: {
-//       type: 'bubble',
-//       body: {
-//         type: 'box',
-//         layout: 'vertical',
-//         contents: [
-//           addressBar(shortenAddress(address)),
-//           tableHeader(),
-//           ...positions.map((position) => poolLine(position)),
-//           summary(totalValue),
-//         ],
-//       },
-//     },
-//   } as any);
-//   return res.sendStatus(200);
-// })
+  await lineClient.replyMessage(replyToken, {
+    type: 'flex',
+    altText: 'Pancake Staking',
+    contents: {
+      type: 'bubble',
+      body: {
+        type: 'box',
+        layout: 'vertical',
+        contents: [
+          addressBar(shortenAddress(address)),
+          tableHeader(),
+          ...positions.map((position) => poolLine(position)),
+          summary(totalValue),
+        ],
+      },
+    },
+  } as any);
+  return res.sendStatus(200);
+})
 
 app.listen(port, () => {
   console.log(`Server is running at https://localhost:${port}`);
